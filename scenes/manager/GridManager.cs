9afb9ec34d7c9359a15d303b164ad0f8651c1e09
KwanLam17 <kwan@gogander.io>
2024-10-15 22:41:33 -0700

    Add ResourceTilesUpdated signal to keep hash count of total collected resources so far

--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -12,7 +12,11 @@ public partial class GridManager : Node
 {
 	private const string IS_BUILDABLE = "is_buildable";
 	private const string IS_WOOD = "is_wood";
+
+	[Signal]
+	public delegate void ResourceTilesUpdatedEventHandler(int collededTiles);
 	private HashSet<Vector2I> validBuildableTiles = new();
+	private HashSet<Vector2I> collectedResourceTiles = new();
 
 	[Export]
 	private TileMapLayer highlightTileMapLayer;
@@ -112,6 +116,18 @@ public partial class GridManager : Node
 		validBuildableTiles.ExceptWith(GetOccupiedTiles());
 	}
 
+	private void UpdateCollectedResourceTiles(BuildingComponent buildingComponent)
+	{
+		var rootCell = buildingComponent.GetGridCellPosition();
+		var resourceTiles = GetResourceTilesInRadius(rootCell, buildingComponent.BuildingResource.ResourceRadius);
+		var oldResoureTileCount = collectedResourceTiles.Count;
+		collectedResourceTiles.UnionWith(resourceTiles);
+		if (oldResoureTileCount != collectedResourceTiles.Count)
+		{
+			EmitSignal(SignalName.ResourceTilesUpdated, collectedResourceTiles.Count);
+		}
+	}
+
 	private List<Vector2I> GetTilesInRadius(Vector2I rootCell, int radius, Func<Vector2I, bool> filterFn)
 	{
 		var result = new List<Vector2I>();
@@ -148,5 +164,6 @@ public partial class GridManager : Node
 	private void OnBuildingPlaced(BuildingComponent buildingComponent)
 	{
 		UpdateValidBuildableTiles(buildingComponent);
+		UpdateCollectedResourceTiles(buildingComponent);
 	}
 }
