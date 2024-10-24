1ddbad59addf9281beed4cb3b559c9b48c7dd0b9
Kwan <kwan@gogander.io>
2024-10-23 21:21:03 -0600

    Make buildable radius circular

--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -63,7 +63,8 @@ public partial class GridManager : Node
 		(TileMapLayer firstTileMaplayer, _) = GetTileCustomData(tiles[0], IS_BUILDABLE);
 		var targetElevationLayer = tileMaplayerToElevationLayer[firstTileMaplayer];
 
-		return tiles.All((tilePosition) => {
+		return tiles.All((tilePosition) =>
+		{
 			(TileMapLayer tileMapLayer, bool isBuildable) = GetTileCustomData(tilePosition, IS_BUILDABLE);
 			var elevationLayer = tileMaplayerToElevationLayer[tileMapLayer];
 			return isBuildable && validBuildableTiles.Contains(tilePosition) && elevationLayer == targetElevationLayer;
@@ -200,15 +201,27 @@ public partial class GridManager : Node
 		EmitSignal(SignalName.GridStateUpdated);
 	}
 
+	private bool IsTileInsideCircle(Vector2 centerPosition, Vector2 tilePosition, float radius)
+	{
+		var distanceX = centerPosition.X - (tilePosition.X + 0.5);
+		var distanceY = centerPosition.Y - (tilePosition.Y + 0.5);
+		var distanceSquared = (distanceX * distanceX) + (distanceY * distanceY);
+
+		return distanceSquared <= radius * radius;
+	}
+
 	private List<Vector2I> GetTilesInRadius(Rect2I tileArea, int radius, Func<Vector2I, bool> filterFn)
 	{
 		var result = new List<Vector2I>();
+		var tileAreaF = tileArea.ToRect2F();
+		var tileAreaCenter = tileAreaF.GetCenter();
+		var radiusMod = Mathf.Max(tileAreaF.Size.X, tileAreaF.Size.Y) / 2;
 		for (var x = tileArea.Position.X - radius; x < tileArea.End.X + radius; x++)
 		{
 			for (var y = tileArea.Position.Y - radius; y < tileArea.End.Y + radius; y++)
 			{
 				var tilePosition = new Vector2I(x, y);
-				if (!filterFn(tilePosition)) continue;
+				if (!IsTileInsideCircle(tileAreaCenter, tilePosition, radius + radiusMod) || !filterFn(tilePosition)) continue;
 				result.Add(tilePosition);
 			}
 		}
